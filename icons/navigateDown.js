60a88ee5adb843570236650c7e967c6486fe71e6
nathanael <nathanael.bjorkgard@billes.se>
2018-04-23 12:12:56 +0200

    Add classname

--- icons/navigateDown.js
+++ icons/navigateDown.js
@@ -4,7 +4,7 @@ import Icon from './icon'
 
 const NavigateDown = ({ viewBox, style }) => (
     <Icon viewBox={viewBox} style={style} className="expand_button">
-        <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" />
+        <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z" className="expand_button" />
         <path d="M0-.75h24v24H0z" fill="none" />
     </Icon>
     )
